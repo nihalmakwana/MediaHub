1fcf630f2116cd473771a045255b2f95f2db3340
@nihalmakwana <makwananihal2308@gmail.com>
2024-11-23 18:06:50 +0530

    getAllUsers, getUserDetails and deleteMedia control

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -2,6 +2,9 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { Admin } from "../models/admin.model.js"
+import { User } from "../models/user.model.js"
+import { Video } from "../models/video.model.js"
+import { Music } from "../models/music.model.js"
 
 const generateAccessAndRefreshToken = async (adminId) => {
     try {
@@ -80,7 +83,103 @@ const adminLogout = asyncHandler( async (req, res) => {
     )
 })
 
+const getAllUsers = asyncHandler( async ( req, res) => {
+    try {
+        const users = await User.find().select("-refreshToken -password")
+        
+        return res 
+        .status(200)
+        .json(
+            new ApiResponse(200, users, "All users details fetched successfully..")
+        )
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Something went wrong while getting all users")
+    }
+})
+
+const getUserDetails = asyncHandler( async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const user = await User.findById(id).select("-password -refreshToken")
+
+        if (!user) {
+            throw new ApiError(404, "User not found")
+        }
+
+        const [videos, musics, likedVideos, dislikedVideo, likedMusic, dislikedMusic] = await Promise.all([
+            Video.find({ userId: id }),
+            Music.find({ userId: id }),
+            Video.find({ _id: { $in: user.likedVideo }}),
+            Video.find({ _id: { $in: user.dislikedVideo }}),
+            Music.find({ _id: { $in: user.likedMusic }}),
+            Music.find({ _id: { $in: user.dislikedMusic }}),
+        ])
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: {
+                        name: user.name,
+                        email: user.email,
+                        avatar: user.avatar,
+                        createdAt: user.createdAt
+                    },
+                    videos,
+                    musics,
+                    likedVideos,
+                    likedMusic,
+                    dislikedVideo,
+                    dislikedMusic
+                },
+                "Get user's all details"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Something went wrong while getting user's details")
+    }
+})
+
+const deleteMedia = asyncHandler( async (req, res) => {
+    const { id } = req.params
+    const { type } = req.query
+
+    try {
+        if (type === 'video') {
+            const deletedVideo = await Video.findByIdAndDelete(id)
+            if (!deletedVideo) {
+                throw new ApiError(404, "Video not found")
+            }
+
+            return res
+            .status(200)
+            .json(
+                new ApiResponse(200, {}, "Video Deleted Successfully!!!")
+            )
+        } else if (type === 'music') {
+            const deletedMusic = await Music.findByIdAndDelete(id)
+            if (!deletedMusic) {
+                throw new ApiError(404, "Music not found")
+            }
+
+            return res
+            .status(200)
+            .json(
+                new ApiResponse(200, {}, "Music Deleted Successfully!!!")
+            )
+        }
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Something went wrong while deleting media")
+    }
+})
+
 export {
     adminLogin,
-    adminLogout
+    adminLogout,
+    getAllUsers,
+    getUserDetails,
+    deleteMedia
 }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { 
     adminLogin, 
-    adminLogout
+    adminLogout,
+    deleteMedia,
+    getAllUsers,
+    getUserDetails
 } from "../controllers/admin.controller.js";
 import { verifyAdminJWT } from "../middlewares/auth.middleware.js";
 
@@ -10,5 +13,9 @@ const router = Router()
 
 router.route("/admin-login").post(adminLogin)
 router.route("/admin-logout").post(verifyAdminJWT, adminLogout)
+
+router.route("/get-all-users").get(verifyAdminJWT, getAllUsers)
+router.route("/get-users-details/:id").get(verifyAdminJWT, getUserDetails)
+router.route("/media/:id").delete(verifyAdminJWT, deleteMedia)
 
 export default router
